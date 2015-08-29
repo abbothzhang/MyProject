b94807bb5c0ff88ef3461152c37d3743dff74787
鹏辉 <penghui.zh@alibaba-inc.com>
2015-08-29 11:02:48 +0800

    add CameraDemo

---
+++ CameraDemo/CameraDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CameraDemo
+//
+//  Created by albert on 15/8/29.
+//  Copyright (c) 2015年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CameraDemo/CameraDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CameraDemo
+//
+//  Created by albert on 15/8/29.
+//  Copyright (c) 2015年 alibaba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
