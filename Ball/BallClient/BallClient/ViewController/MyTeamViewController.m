fedd73dfa5b4024ed9dfc19bbe294104cab4c226
鹏辉 <penghui.zh@alibaba-inc.com>
2015-09-10 21:28:58 +0800

    设置nav背景为图片

--- Ball/BallClient/BallClient/ViewController/MyTeamViewController.m
+++ Ball/BallClient/BallClient/ViewController/MyTeamViewController.m
@@ -16,22 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    UIImage *navBgImg = [UIImage imageNamed:@"navbg"];
+    [self.navigationController.navigationBar setBackgroundImage:navBgImg forBarMetrics:UIBarMetricsDefault];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
